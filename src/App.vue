e32a0e689277a52c6e84284213f37eeb25fae749
Misha Marinenko <marinenko.m@d2.digital>
2020-05-18 23:12:24 +0300

    WIP: discord

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,22 @@
 
 <script>
 import { Notify } from "quasar";
+import RPC from "discord-rpc";
 export default {
   name: "App",
   mounted() {
+    const clientId = "712033097323118592";
+    const scopes = ["rpc", "rpc.api", "messages.read"];
+
+    const client = new RPC.Client({ transport: "websocket" });
+
+    client.on("ready", () => {
+      console.log("Logged in as", client.application.name);
+      console.log("Authed for user", client.user.username);
+    });
+
+    // Log in to RPC with client id
+    client.login({ clientId, scopes });
     // window["isUpdateAvailable"].then(isAvailable => {
     //   if (isAvailable) {
     //     Notify.create("Доступно обновление перезайди 😉");
